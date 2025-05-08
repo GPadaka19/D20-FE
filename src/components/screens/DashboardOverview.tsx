bfdcd473dd4668eb2f68ac5fdb47debdd8caa137
GPadaka19 <gustipadaka19@gmail.com>
2025-05-08 21:18:25 +0700

    Test GA #4

--- src/components/screens/DashboardOverview.tsx
+++ src/components/screens/DashboardOverview.tsx
@@ -114,7 +114,7 @@ const DashboardOverview: React.FC = () => {
             <TEChart
               type="line"
               data={{
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+                labels: ["Jan", "Feb", "Mar", "Apr", "May", "DESSSSSSSS"],
                 datasets: [
                   {
                     label: "Confidence Level",
